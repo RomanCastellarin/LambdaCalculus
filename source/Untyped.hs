2f235ea188f0f2f9a6285841108bd54e5a28b0b9
pepito <pepito@asd>
2018-10-16 13:35:18 -0300

    ex 4 and partially 5, @romancastellarin @juaniisuar

--- source/Untyped.hs
+++ source/Untyped.hs
@@ -28,15 +28,32 @@ conversion = \x -> convTerms x []
 shift :: Term -> Int -> Int -> Term
 shift (Free x)    c d = Free x
 shift (Bound x)   c d | x < c     = Bound x
-                      | otherwise = Bound $ x + d
+                      | otherwise = Bound (x + d)
 shift (t1 :@: t2) c d = (shift t1 c d) :@: (shift t2 c d)
 shift (Lam t)     c d = Lam $ shift t (c+1) d
 
+-------------------------------
 
 subst :: Term -> Term -> Int -> Term
-subst = undefined
+subst (Free k)     t' i = Free k
+subst (Bound k)    t' i | k == i    = t'
+                        | otherwise = Bound k
+subst (t1 :@: t2)  t' i = (subst t1 t' i) :@: (subst t2 t' i)
+subst (Lam t)      t' i = Lam $ subst t (shift t' 1 0) (i+1)
 
 
 eval :: NameEnv Term -> Term -> Term
-eval = undefined
 
+eval nvs (Free k)    = case lookup k nvs of
+                          Just m  -> eval nvs m
+                          Nothing -> Free k -- error $ "unregistered variable " ++ k
+eval nvs (Bound k)   = Bound k
+-- E-ABS
+eval nvs (Lam t)     = Lam $ eval nvs t
+-- E-AppAbs
+eval nvs ((Lam t1) :@: t2)    = eval nvs $ shift (subst t1 (shift t2 0 1) 0) 0 (-1) 
+
+---------
+
+eval nvs ((t1 :@: t2) :@: t3) = ?????
+                                
