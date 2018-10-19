b7114f91b51270576a1e09e62daa62bee7607e64
Juan Ignacio Suarez <juaniisuar@gmail.com>
2018-10-18 21:12:27 -0300

    Ahora si, ej 5 y 6

--- source/Untyped.hs
+++ source/Untyped.hs
@@ -39,23 +39,20 @@ subst (Free k)     t' i = Free k
 subst (Bound k)    t' i | k == i    = t'
                         | otherwise = Bound k
 subst (t1 :@: t2)  t' i = (subst t1 t' i) :@: (subst t2 t' i)
-subst (Lam t)      t' i = Lam $ subst t (shift t' 1 0) (i+1)
+subst (Lam t)      t' i = Lam $ subst t (shift t' 0 1) (i+1)
 
+-------------------------------
 
 eval :: NameEnv Term -> Term -> Term
-
 eval nvs (Free k)          = case lookup k nvs of
                                 Just m  -> eval nvs m
-                                Nothing -> Free k -- error $ "unregistered variable " ++ k
+                                Nothing -> Free k
 eval nvs (Bound k)         = Bound k
 -- E-ABS
 eval nvs (Lam t)           = Lam $ eval nvs t
 -- E-AppAbs
 eval nvs ((Lam t1) :@: t2) = eval nvs $ shift (subst t1 (shift t2 0 1) 0) 0 (-1) 
--- 
+-- E-App1/App2
 eval nvs (t1 :@: t2)       = case eval nvs t1 of
-	                            Lam t3 -> eval nvs $ Lam t3 :@: t2
-                                t3     -> t3 :@: eval nvs t2
-
-
-                                
+                                Lam t3 -> eval nvs $ Lam t3 :@: t2
+                                t3 -> t3 :@: eval nvs t2
